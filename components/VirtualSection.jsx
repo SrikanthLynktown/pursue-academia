6cc24c67f9b040febf8e85add1838b2016c2f90f
Motiar Rahaman <rahamanmotiar70@gmail.com>
2023-03-24 11:16:26 +0530

    caraosu

--- components/VirtualSection.jsx
+++ components/VirtualSection.jsx
@@ -13,17 +13,17 @@ const VirtualSection = () => {
         <section className="text-gray-600 body-font">
           <div className="container mx-auto flex px-5 md:flex-row flex-col items-center">
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0  text-center space-y-5">
-              <h1 className="px-4 md:px-0 text-[#313131] text-[30px] md:text-[48px] w-[345px] md:w-[680px] h-[100px] md:h-[125px] leading:[42px] md:leading-[64px] text-left font-medium	font-serif font-semibold not-italic  max-w-[680px]">
+            <h1 className="text-[#313131] text-[30px] md:text-[48px] w-[345px] md:w-[680px] h-[125px] md:h-[128px] leading:[42px] md:leading-[64px] text-left font-medium	font-serif font-semibold not-italic  max-w-[680px]">
                 Virtual Internship Program
               </h1>
-              <p className="px-4 md:px-0 text-[#71717A] text-left text-[15px] md:text-[24px]  w-[345px] md:w-[680px] h-[62px] md:h-[96px] leading-[21px] md:leading-[32px] font-serif font-normal not-italic">
+              <p className="md:px-0 text-[#71717A] text-left text-[15px] md:text-[24px]  w-[345px] md:w-[680px] h-[62px] md:h-[96px] leading-[21px] md:leading-[32px] font-serif font-normal not-italic">
                 Expert-curated internships program enables students to try out careers before making the career decision. Thanks to internet
               </p>
               <div className="space-y-4">
-                <div className="bg-[#3780FF] w-[260px] h-[31px] md:w-[400px] md:h-[48px] rounded-[5px] md:rounded-[8px] flex mx-auto justify-center items-center cursor-pointer" onClick={() => window.open('https://legislative.gov.in/sites/default/files/dummy-pdf_2_2.pdf')}>
+                <div className="bg-[#3780FF] w-[260px] h-[31px] md:w-[400px] md:h-[48px] rounded-[5px] md:rounded-[8px] flex mx-au justify-center items-center cursor-pointer" onClick={() => window.open('https://legislative.gov.in/sites/default/files/dummy-pdf_2_2.pdf')}>
                   <span className=" text-[#FFFFFF] text-[10px] md:text-[16px] w-[98px] h-[16px]   md:w-[155px] md:h-[24px] leading-[16px] md:leading-[24px]  font-serif font-medium not-italic"> Download the fllyer</span>
                 </div>
-                <div className="relative w-[260px] h-[44px] md:w-[400px] md:h-[51px] mx-auto">
+                <div className="relative w-[260px] h-[44px] md:w-[400px] md:h-[51px]">
                   <Image
                     src="/Whatsapp button.png"
                     className="mt-[15.01px]"
